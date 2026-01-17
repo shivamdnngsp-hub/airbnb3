9efb4ed449a57e37a69540a892441ee8acaa2eae
Shivam <shivamdnngsp@gmail.com>
2026-01-18 00:04:52 +0530

    Update bottomMobile.jsx

--- frontend/src/components/bottomMobile.jsx
+++ frontend/src/components/bottomMobile.jsx
@@ -5,39 +5,39 @@ import { useNavigate } from "react-router-dom";
 const BottomMobile = () => {
     const navigate = useNavigate()
     return (
-        <div className="fixed bottom-0 z-20 flex sm:hidden bg-white h-15 justify-between items-center pl-10 pr-10">
+      <div className="
+      fixed bottom-0 left-0 right-0
+      z-50
+      flex sm:hidden
+      bg-white/95 backdrop-blur
+      h-17.5
+      justify-between items-center
+      px-10
+      border-t
+    ">
 
+      <button
+        className="border rounded-full p-2 hover:shadow-md transition"
+        onClick={() => navigate("/wishlist")}
+      >
+        <CiHeart className="text-lg" />
+      </button>
 
+      <button
+        className="border rounded-full p-2 hover:shadow-md transition"
+        onClick={() => navigate("/Add-listing")}
+      >
+        <MdAddHome className="text-lg" />
+      </button>
 
-            <button
-                className="border rounded-full p-2 hover:shadow-md transition flex items-center gap-2 cursor-pointer"
-                onClick={() => navigate("/wishlist")}
-            >
-                <CiHeart className="text-lg" />
-            </button>
+      <button
+        className="border rounded-full p-2 hover:shadow-md transition"
+        onClick={() => navigate("/profile")}
+      >
+        <VscAccount className="text-lg" />
+      </button>
 
-
-
-
-            <button
-                className="border rounded-full p-2 hover:shadow-md transition flex items-center gap-2 cursor-pointer "
-                onClick={() => navigate("/Add-listing")}
-            >
-                <MdAddHome className="text-lg" />
-            </button>
-
-
-
-
-            <button
-                className="border rounded-full p-2 hover:shadow-md transition flex items-center gap-2 cursor-pointer"
-                onClick={() => navigate("/profile")}
-            >
-                <VscAccount className="text-lg" />
-            </button>
-
-
-        </div>
+    </div>
     )
 
 
