5fe145293b3e01ac74c0f31d765c058f873d4e2b
Shivam <shivamdnngsp@gmail.com>
2026-01-18 01:45:40 +0530

    Update axios.jsx

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+
+axios.defaults.withCredentials = true;
+
 const api = axios.create({
-   baseURL: "https://airbnb-backend-xpor.onrender.com/api",
-  withCredentials: true
+  baseURL: "https://airbnb-backend-xpor.onrender.com/api",
+  withCredentials: true,
 });
 
 export default api;
